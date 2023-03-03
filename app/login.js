716686d1c5c5ba318137a361adadf4b2b5655c8e
theJuntoClub <thjuntoclub@gmail.com>
2023-03-03 00:18:45 -0300

    ?

---
+++ app/login.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#login-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // conseguir el email y la contra del input del usuario
+  const email = document.querySelector("#email").value;
+  const pass = document.querySelector("#pass").value;
+  // chequear si el email y la contra coicidan ("match") con los inputs
+  if (email === "thjuntoclub@gmail.com" && pass === "1234") {
+    // si coinciden, ingresamos a nuestra app final
+    window.location.href = "../pages/login.html";
+    alert("si! ya sos parte de la comunidad!! 👍");
+  } else {
+    alert("ups, tus datos no coinciden 👎");
+  }
+});
