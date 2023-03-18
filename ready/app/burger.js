dec5ac57d556372338df77353188b360cf46747b
theJuntoClub <thjuntoclub@gmail.com>
2023-03-18 19:37:11 -0300

    ?

--- ready/app/burger.js
+++ ready/app/burger.js
@@ -4,4 +4,18 @@ const myChat = document.getElementById("chat");
 myBurgerChat.addEventListener("click", () => {
   myChat.classList.toggle("show-chat");
   myBurgerChat.classList.toggle("show-x");
+  const showX = false;
+  /*   if (!showX) {
+    myBurgerChat.classList.toggle("show-x");
+    showX = true;
+  } */
+});
+
+// sweet alert
+Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "Sesión iniciada 🔥",
+  showConfirmButton: false,
+  timer: 1500,
 });
